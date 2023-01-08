9f4540d473f083fe0940c76e2168807fddd64f8d
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-01-08 21:40:35 +0200

    change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+// console.log(Player);
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
 
+player.on('play', function() {
+    console.log('played the video!');
+});
