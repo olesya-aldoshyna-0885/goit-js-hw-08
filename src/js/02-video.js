88f3b9db13b82d95299c9331341aa1f41329fe55
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-01-15 14:22:21 +0200

    1 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,5 @@
 import Player from '@vimeo/player';
-// console.log(Player);
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+// console.log(Player);
+console.log(galleryItems);
+
