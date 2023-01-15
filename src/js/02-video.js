1612861e7920baba28a8ab7697bf0b827d1a18ab
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-01-15 16:55:32 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,26 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
-// console.log(Player);
-console.log(galleryItems);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+LOCALE_KEY = `videoplayer-current-time`;
 
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(LOCALE_KEY, data.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem(LOCALE_KEY))
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
